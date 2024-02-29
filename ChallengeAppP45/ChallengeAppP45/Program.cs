6a2a43b973d4ebf191081c65dcd9843cf17177f0
P45K <160267533+P45K@users.noreply.github.com>
2024-02-29 22:30:38 +0100

    Dzień 4 - Poprawka poprawki zadania domowego

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,18 +1,17 @@
 ﻿// Dzień 4 - Zadanie Domowe
-var Name = "Gabriel";
-var Sex = 'M'; // M = male, F = female
-var Age = 8;
+var name = "Gabriel";
+var sex = 'M'; // M = male, F = female
+var age = 8;
 
-if (Sex == 'F' && Age < 30)
+if (sex == 'F' && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-
-else if (Name == "Ewa" && Age == 30)
+else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else if (Sex == 'M' && Age < 18)
+else if (sex == 'M' && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
