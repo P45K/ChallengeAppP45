6b8ba281c101ca6fec4a7d8fd9f8ef3e67ba1442
P45K <160267533+P45K@users.noreply.github.com>
2024-02-29 22:01:14 +0100

    Dzień 4  - Poprawione zadanie domowe

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,22 +1,18 @@
 ﻿// Dzień 4 - Zadanie Domowe
-var varName = "Gabriel";
-var varSex = 'M'; // M = male, F = female
-var varAge = 8;
+var Name = "Gabriel";
+var Sex = 'M'; // M = male, F = female
+var Age = 8;
 
-if (varSex == 'F' && varAge < 30)
+if (Sex == 'F' && Age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
 
-else if (varName == "Ewa" && varAge == 30)
+else if (Name == "Ewa" && Age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else if (varSex == 'M' && varAge < 18)
+else if (Sex == 'M' && Age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    // pusty blok else - nie zostały spełnione żadne warunki, dlatego program nie zwrócił komunikatu
 }
