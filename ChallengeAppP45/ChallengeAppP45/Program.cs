31e162bed6a459ba03ebd8f819de9295d24bf238
P45K <160267533+P45K@users.noreply.github.com>
2024-03-01 15:40:14 +0100

    Dzień 5 - Pętla for

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,4 +1,4 @@
-﻿// DZIEŃ 5 - [] TABLICE I ICH UŻYCIE
+﻿// DZIEŃ 5 - PĘTLA FOR
 
 int[] grades = new int[5]; // (eng)grades -> (pl)oceny / var grades = new int[5]; - też dobrze
 string[] dayOfWeeks = new string[7];
@@ -9,9 +9,8 @@ dayOfWeeks[3] = "czwartek";
 dayOfWeeks[4] = "piątek";
 dayOfWeeks[5] = "sobota";
 dayOfWeeks[6] = "niedziela";
-Console.WriteLine(dayOfWeeks[2]);
 
-/* (inny sposób na to co powyżej)
-string[] dayOfWeeks2 = { "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela" };
-Console.WriteLine(dayOfWeeks2[0]);
-*/
+for(var i=0; i<=45; i++)  // gdzie: var i=0; to inicjalizacja zmiennej typu "int" / i<=45; to warunek który musi zostać spełniony, aby pętla kontynuowała swoje działanie / i++ (inkrementacja) to instrukcja wykonana po każdym przebiegu pętli (zwiększa wartość "i" o jeden)  
+{
+    Console.WriteLine(i);
+}
