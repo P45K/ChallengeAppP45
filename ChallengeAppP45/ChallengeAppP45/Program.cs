5118602c407500337497a2d3b28e6c6d05fd329e
P45K <160267533+P45K@users.noreply.github.com>
2024-03-01 16:01:06 +0100

    Dzień 5 - Połączenie tablic i pętli for

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,7 +1,7 @@
-﻿// DZIEŃ 5 - PĘTLA FOR
+﻿// DZIEŃ 5 - POŁĄCZENIE TABLIC I PĘTLI FOR
 
-int[] grades = new int[5]; // (eng)grades -> (pl)oceny / var grades = new int[5]; - też dobrze
-string[] dayOfWeeks = new string[7];
+int[] grades = new int[5];
+string[] dayOfWeeks = new string[7]; 
 dayOfWeeks[0] = "poniedziałek";
 dayOfWeeks[1] = "wtorek";
 dayOfWeeks[2] = "środa";
@@ -10,7 +10,7 @@ dayOfWeeks[4] = "piątek";
 dayOfWeeks[5] = "sobota";
 dayOfWeeks[6] = "niedziela";
 
-for(var i=0; i<=45; i++)  // gdzie: var i=0; to inicjalizacja zmiennej typu "int" / i<=45; to warunek który musi zostać spełniony, aby pętla kontynuowała swoje działanie / i++ (inkrementacja) to instrukcja wykonana po każdym przebiegu pętli (zwiększa wartość "i" o jeden)  
+for(var i=0; i<dayOfWeeks.Length; i=i+2) // gdzie: "i<dayOfWeeks.Length;" zwraca długości (ilość elementów) tablicy "dayOfWeeks" z 4 lini kodu / "i=i+2" zwiększa wartość "i" o dwa
 {
-    Console.WriteLine(i);
+    Console.WriteLine(dayOfWeeks[i]);
 }
