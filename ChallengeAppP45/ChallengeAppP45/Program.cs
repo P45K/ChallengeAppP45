22a0b142b070f17801bdfcf7ac0025c5a07a03b8
P45K <160267533+P45K@users.noreply.github.com>
2024-03-02 21:35:42 +0100

    Dzień 6 - Wiele konstruktorów

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,9 +1,7 @@
-﻿User user1 = new User("Przemek");
-User user2 = new User("Gabriel");
-User user3 = new User("Julia");
+﻿User user1 = new User();    // wywołanie "user1" bez parametrów
+User user2 = new User("Gabriel", "");   // wywołanie "user2" z dwoma parametrami
+User user3 = new User("Julia");     // wywołanie "user2" z jednym parametrem 
 User user4 = new User("Wioleta");
-
-
 
 class User
 {
@@ -11,8 +9,24 @@ class User
     private string password;
     private string name;
 
-    public User(string login)               // tworzenie konstruktowa dla klasy "User", z parametrem "login", który jest typu (string)
+    public User()   // konstruktor bez parametrów 
+    {
+        this.login = "-";
+        this.password = "-";
+        this.name = "-";
+    }
+
+    public User(string login)   // konstruktor z jednym parametrem
     {
         this.login = login;
+        this.password = "-";
+        this.name = "-";
+    }
+
+    public User(string login, string password)  // konstruktor z dwoma parametrami
+    {
+        this.login = login;
+        this.password = password;
+        this.name = "-";
     }
 }
