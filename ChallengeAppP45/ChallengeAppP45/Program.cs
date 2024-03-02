27c164a5113978c646bf41655bb240fb0a62496c
P45K <160267533+P45K@users.noreply.github.com>
2024-03-02 22:09:05 +0100

    Dzień 6 - Właściwości klasy get i set

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,32 +1,18 @@
-﻿User user1 = new User();    // wywołanie "user1" bez parametrów
-User user2 = new User("Gabriel", "");   // wywołanie "user2" z dwoma parametrami
-User user3 = new User("Julia");     // wywołanie "user2" z jednym parametrem 
-User user4 = new User("Wioleta");
+﻿User user1 = new User("Przemek", "12345");
+User user2 = new User("Gabriel", "54321");
+User user3 = new User("Julia", "67890");
+User user4 = new User("Wioleta", "09876");
+
+var name = user1.Login; 
 
 class User
 {
-    private string login;
-    private string password;
-    private string name;
-
-    public User()   // konstruktor bez parametrów 
+    public User(string login, string password)
     {
-        this.login = "-";
-        this.password = "-";
-        this.name = "-";
+        this.Login = login;
+        this.Password = password;
     }
 
-    public User(string login)   // konstruktor z jednym parametrem
-    {
-        this.login = login;
-        this.password = "-";
-        this.name = "-";
-    }
-
-    public User(string login, string password)  // konstruktor z dwoma parametrami
-    {
-        this.login = login;
-        this.password = password;
-        this.name = "-";
-    }
+    public string Login { get; private set }     // propercja (property) - właściwości klasy, gdzie (get) zwraca wartość, a (set) ustawia wartość, w tym przypadku set jest dostępne tylko wewnatrz klasy 
+    public string Password { get; private set }
 }
