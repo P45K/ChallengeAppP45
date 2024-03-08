77424ca947a063df519cbd846e3fb4f983e4f302
P45K <160267533+P45K@users.noreply.github.com>
2024-03-08 15:38:38 +0100

    Dzień 6 - Zmienne i metody statyczne

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,4 +1,4 @@
-﻿User user1 = new User("Przemek", "12345");
+﻿User user1 = new User("Przemek", "12345");  // "User" jest typem obiektu "user1"
 User user2 = new User("Gabriel", "54321");
 User user3 = new User("Julia", "67890");
 User user4 = new User("Wioleta", "09876");
@@ -6,14 +6,17 @@ User user4 = new User("Wioleta", "09876");
 user1.AddScore(5);
 user1.AddScore(3);
 user1.AddScore(7);
-
 var result = user1.Result;
-
 Console.WriteLine(result);
+var name = User.GameName;
 
 
-class User
+
+//  KLASA
+class User 
 {
+    public static string GameName = "Diablo";   // zmienna statyczna 
+
     private List<int> score = new List<int>();
 
     //  KONSTRUKTOR
@@ -24,7 +27,7 @@ class User
     }
 
     //  POLA - parametry opisuące nasz obiekt
-    public string Login { get; private set; }     // propercja (property) - właściwości klasy, gdzie (get) zwraca wartość, a (set) ustawia wartość, w tym przypadku set jest dostępne tylko wewnatrz klasy 
+    public string Login { get; private set; }   // propercja (property) - właściwości klasy, gdzie (get) zwraca wartość, a (set) ustawia wartość, w tym przypadku set jest dostępne tylko wewnatrz klasy 
     public string Password { get; private set; }
 
     public int Result
