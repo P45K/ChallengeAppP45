6672d3a8ac88bdd431ba15b174225bd728699f97
P45K <160267533+P45K@users.noreply.github.com>
2024-03-08 14:45:47 +0100

    Dzień 6 - Metody

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -3,16 +3,28 @@ User user2 = new User("Gabriel", "54321");
 User user3 = new User("Julia", "67890");
 User user4 = new User("Wioleta", "09876");
 
-var name = user1.Login; 
+user1.AddScore(5);
 
 class User
 {
+    private int score;
+
+    //  KONSTRUKTOR
     public User(string login, string password)
     {
         this.Login = login;
         this.Password = password;
+        this.score = 0;
     }
 
+    //  POLA - parametry opisuące nasz obiekt
     public string Login { get; private set; }     // propercja (property) - właściwości klasy, gdzie (get) zwraca wartość, a (set) ustawia wartość, w tym przypadku set jest dostępne tylko wewnatrz klasy 
     public string Password { get; private set; }
+
+    //  METODA 
+
+    public void AddScore(int number) 
+    {
+        this.score += number;
+    }
 }
