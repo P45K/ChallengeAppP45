958a1c348be2f5f43323ca034822a192766e1c71
P45K <160267533+P45K@users.noreply.github.com>
2024-03-08 15:19:00 +0100

    Dzień 6 - Metody i hermetyzacja

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -4,27 +4,40 @@ User user3 = new User("Julia", "67890");
 User user4 = new User("Wioleta", "09876");
 
 user1.AddScore(5);
+user1.AddScore(3);
+user1.AddScore(7);
+
+var result = user1.Result;
+
+Console.WriteLine(result);
+
 
 class User
 {
-    private int score;
+    private List<int> score = new List<int>();
 
     //  KONSTRUKTOR
     public User(string login, string password)
     {
         this.Login = login;
         this.Password = password;
-        this.score = 0;
     }
 
     //  POLA - parametry opisuące nasz obiekt
     public string Login { get; private set; }     // propercja (property) - właściwości klasy, gdzie (get) zwraca wartość, a (set) ustawia wartość, w tym przypadku set jest dostępne tylko wewnatrz klasy 
     public string Password { get; private set; }
 
-    //  METODA 
-
-    public void AddScore(int number) 
+    public int Result
     {
-        this.score += number;
+        get     // 
+        {
+            return this.score.Sum();    // metoda "Sum" sumuje wszystkie punkty(liczby) na liście
+        }
+    }
+
+    //  METODA
+    public void AddScore(int number)    // metoda "AddScore" dodaje kolejne liczby do listy "score" 
+    {
+        this.score.Add(number);
     }
 }
