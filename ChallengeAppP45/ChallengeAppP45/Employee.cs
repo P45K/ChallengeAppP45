f0004266e6d769de06736b4d064a4e63673bde1c
P45K <160267533+P45K@users.noreply.github.com>
2024-03-09 21:23:27 +0100

    Dzień 6 - Zadanie domowe

---
+++ ChallengeAppP45/ChallengeAppP45/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace ChallengeAppP45
+{
+    // KLASA
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+        // KONSTRUKTOR
+        public Employee(string name, string surName, string age)
+        {
+            this.Name = name;
+            this.SurName = surName;
+            this.Age = age;
+        }
+
+        // POLA
+        public string Name { get; private set; }
+        public string SurName { get; private set; }
+        public string Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        // METODA
+
+        public void AddPoints(int points)
+        {
+            this.score.Add(points);
+        }
+    }
+}
--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,13 +1,80 @@
 ﻿using ChallengeAppP45;
 
-User user1 = new User("Przemek", "12345");  // "User" jest typem obiektu "user1"
-User user2 = new User("Gabriel", "54321");
-User user3 = new User("Julia", "67890");
-User user4 = new User("Wioleta", "09876");
+Employee employee1 = new Employee("Edek", "Zkrainykredek", "22");   // "Employee" jest typem obiektu "employee1"
+Employee employee2 = new Employee("Janek", "Rambo", "55");
+Employee employee3 = new Employee("Franek", "Mistrzkarate", "44");
+Employee employee4 = new Employee("Bob", "Mistrzreggae", "37");
+Employee employee5 = new Employee("Zenek", "Rezyserkinaakcji", "33");
 
-user1.AddScore(5);
-user1.AddScore(3);
-user1.AddScore(7);
-var result = user1.Result;
-Console.WriteLine(result);
-// var name = User.GameName;
+List<Employee> allEmployees = new List<Employee>() 
+{ 
+    employee1, employee2, employee3, employee4, employee5 
+};
+
+employee1.AddPoints(6);
+employee1.AddPoints(8);
+employee1.AddPoints(7);
+employee1.AddPoints(9);
+employee1.AddPoints(5);
+
+employee2.AddPoints(8);
+employee2.AddPoints(9);
+employee2.AddPoints(7);
+employee2.AddPoints(10);
+employee2.AddPoints(7);
+
+employee3.AddPoints(7);
+employee3.AddPoints(8);
+employee3.AddPoints(9);
+employee3.AddPoints(4);
+employee3.AddPoints(8);
+
+employee4.AddPoints(7);
+employee4.AddPoints(7);
+employee4.AddPoints(10);
+employee4.AddPoints(8);
+employee4.AddPoints(9);
+
+employee5.AddPoints(7);
+employee5.AddPoints(4);
+employee5.AddPoints(9);
+employee5.AddPoints(10);
+employee5.AddPoints(8);
+
+int bestScore = 0;
+
+List<Employee> topEmployees = new List<Employee>();
+
+foreach (var bestEmployee in allEmployees)    // pętla przechodzi przez wszystkich pracowników z listy "allEmployees", szuka najwyższego wyniku "bestScore" i dodaje go do listy "topEmployees"
+{
+    if (bestEmployee.Result == bestScore)
+    {
+        topEmployees.Add(bestEmployee);
+    }
+    else if (bestEmployee.Result > bestScore) // kiedy kolejny wynik jest większy od aktualnego, lista "topEmployee" zostaje wyczyszczona, następnie zaktualizowana
+    {
+        bestScore = bestEmployee.Result;
+        topEmployees.Clear();
+        topEmployees.Add(bestEmployee);
+    }
+}
+
+Console.WriteLine("-------------------------------");
+Console.WriteLine("     B E S T    S C O R E:");
+Console.WriteLine("-------------------------------");
+foreach (var winnerEmployee in topEmployees)    // pętla ma za zadanie wyświetlić najlepsze wyniki z listy "topEmployees"
+{
+    Console.WriteLine(winnerEmployee.Name + " " + winnerEmployee.SurName + " - " + winnerEmployee.Result + "pkt");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("-------------------------------");
+Console.WriteLine("     A L L    S C O R E S:");
+Console.WriteLine("-------------------------------");
+
+foreach (var restEmployees in allEmployees.OrderByDescending(x => x.Result))    // tutaj pętla sortuje wszystkie wyniki od największej do najmniejszej liczby pkt
+{
+    Console.WriteLine(restEmployees.Name + " " + restEmployees.SurName + " - " + restEmployees.Result + "pkt");
+}
