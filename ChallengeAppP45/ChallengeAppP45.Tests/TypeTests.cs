d5be7fad80e45bc2925521766aafc640d5035475
P45K <160267533+P45K@users.noreply.github.com>
2024-03-14 10:51:24 +0100

    Dzień 8 - Typy referencyjne

--- ChallengeAppP45/ChallengeAppP45.Tests/TypeTests.cs
+++ ChallengeAppP45/ChallengeAppP45.Tests/TypeTests.cs
@@ -6,14 +6,15 @@
         public void TEST()
         {
             // arrange
-            var user1 = GetUser("Edziu");
-            var user2 = GetUser("Władziu");
+            var user1 = GetUser("Edziu");       // gdzie "user1" i user2" to typ referencjyjny, zajmują różne miejsce w pamięci(są różne)
+            var user2 = GetUser("Edziu");       // gdzie "Edziu" i "Władziu" to typ wartościowy
+            var user3 = GetUser("Władziu");
 
             // act
 
 
             // assert
-
+            Assert.AreEqual(user1.Login, user2.Login);
         }
 
         private User GetUser(string name) 
