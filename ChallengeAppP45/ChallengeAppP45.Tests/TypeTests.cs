a0a15d3d596bd926e257765a42cbbf60dc74d63a
P45K <160267533+P45K@users.noreply.github.com>
2024-03-14 11:02:21 +0100

    Dzień 8 - Typy wartościowe

--- ChallengeAppP45/ChallengeAppP45.Tests/TypeTests.cs
+++ ChallengeAppP45/ChallengeAppP45.Tests/TypeTests.cs
@@ -6,15 +6,29 @@
         public void TEST()
         {
             // arrange
+            int number1 = 1;    // int, double, float, byte, long, decimal, short to typy wartościowe
+            int number2 = 2;
+
+            // act
+
+
+            // assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void GetUserShouldReturnDifferentObjects()
+        {
+            // arrange
             var user1 = GetUser("Edziu");       // gdzie "user1" i user2" to typ referencjyjny, zajmują różne miejsce w pamięci(są różne)
-            var user2 = GetUser("Edziu");       // gdzie "Edziu" i "Władziu" to typ wartościowy
+            var user2 = GetUser("Edziu");       // gdzie "Edziu" i "Władziu" to typ wartościowy (łańcuch znaków)
             var user3 = GetUser("Władziu");
 
             // act
 
 
             // assert
-            Assert.AreEqual(user1.Login, user2.Login);
+            Assert.AreNotEqual(user1, user2);
         }
 
         private User GetUser(string name) 
