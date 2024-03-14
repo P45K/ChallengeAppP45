f8c2f742bde4808dbbc7996e289e8892be98ddc0
P45K <160267533+P45K@users.noreply.github.com>
2024-03-14 10:30:24 +0100

    Dzień 8 - Czym są typy referencyjne i wartościowe?

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,4 +1,19 @@
-﻿using ChallengeAppP45;
+﻿//  TYPY REFERENCYJNY I WARTOŚCIOWE
+
+//  TYP REFERENCYJNY                TYP WARTOŚCIOWY
+/* 
+    var emp = new                   int number = 12;
+    Employee("Edziu");
+    
+    - w zmiennej "emp" przechowujemy obiekt(referencje) do obiektu "Employee"
+    - w typie wartościowym (typ prosty) przechowujemy wartość np "12"
+    - w przypadku typów referencyjnych przechowujemy miejsce w pamięci(adres), gdzie nasz program wie, że tam ma przechowywać obiekt typu "Employee"
+    - obydwa typy są przechowywane w uporządkowanej strukturze w pamięci, która nazywa się "STOSEM"
+    - obiekt "Employee" przechowywany jest w "STERCIE" 
+ */
+
+
+using ChallengeAppP45;
 
     User user1 = new User("Przemek", "12345");
     User user2 = new User("Gabriel", "54321");
