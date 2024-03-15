f2b5e53e2246e975e3b592d0d76cdeeba9bb49c9
P45K <160267533+P45K@users.noreply.github.com>
2024-03-15 14:04:43 +0100

    Dzień 9 - zwracanie obiektów

--- ChallengeAppP45/ChallengeAppP45/Employee.cs
+++ ChallengeAppP45/ChallengeAppP45/Employee.cs
@@ -3,34 +3,33 @@
     // KLASA
     public class Employee
     {
-        private List<int> score = new List<int>();
+        // LISTA
+        private List<float> grades = new List<float>();
 
         // KONSTRUKTOR
-        public Employee(string name, string surName, string age)
+        public Employee(string firstName, string lastName)
         {
-            this.Name = name;
-            this.SurName = surName;
-            this.Age = age;
+            this.FirstName = firstName;
+            this.LastName = lastName;
         }
 
         // POLA
-        public string Name { get; private set; }
-        public string SurName { get; private set; }
-        public string Age { get; private set; }
-
-        public int Result
-        {
-            get
-            {
-                return this.score.Sum();
-            }
-        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         // METODA
-
-        public void AddPoints(int points)
+        public void Addgrade(float grade) 
         {
-            this.score.Add(points);
+            this.grades.Add(grade);
+        }
+
+        public Statistics GetStatistics() 
+        {
+            var statistics = new Statistics();
+
+
+
+            return statistics;
         }
     }
 }
--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,34 +1,5 @@
-﻿//  TYPY REFERENCYJNY I WARTOŚCIOWE
+﻿using ChallengeAppP45;
 
-//  TYP REFERENCYJNY                TYP WARTOŚCIOWY
-/* 
-    var emp = new                   int number = 12;
-    Employee("Edziu");
-    
-    - w zmiennej "emp" przechowujemy obiekt(referencje) do obiektu "Employee"
-    - w typie wartościowym (typ prosty) przechowujemy wartość np "12"
-    - w przypadku typów referencyjnych przechowujemy miejsce w pamięci(adres), gdzie nasz program wie, że tam ma przechowywać obiekt typu "Employee"
-    - obydwa typy są przechowywane w uporządkowanej strukturze w pamięci, która nazywa się "STOSEM"
-    - obiekt "Employee" przechowywany jest w "STERCIE" 
-    - najprostrzym sposobem na ropzpoznanie z jakim typem mamy do czynienia jest przejście do implementacji i tam jeżeli nie ma słówka "class", to znaczy, że mamy do czynienia z typem wartościowym
-    - STRING jest typem referencyjnym ale zachowuje się jak typ wartościowy - jest hybrydą (immutable - niezmienny)
- */
-
-
-using ChallengeAppP45;
-
-    User user1 = new User("Władziu", "567890", "Władysław");
-    User user2 = new User("Władek", "765432", "Władysław");
-    User user3 = new User("Tadziu", "67890", "Tadeusz", 1985);
-    User user4 = new User("Edziu", "09876", "Edward", 1980);
-    User user5 = new User("Radziu", "99988", "Radosław", 1979);
-
-    user1.AddPositivePoints(5);
-    user1.AddPositivePoints(3);
-    user1.AddPositivePoints(7);
-    user1.AddNegativePoints(-1);
-
-    user2.AddPositivePoints(5);
-    user2.AddPositivePoints(3);
-    user2.AddPositivePoints(7);
-    user2.AddNegativePoints(-1);
+var employee = new Employee("Władziu", "Władysławski");
+employee.Addgrade(2);
+var statistics = employee.GetStatistics();
