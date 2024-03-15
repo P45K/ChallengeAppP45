689c6b826ad521f927007a264acd22fd2e052410
P45K <160267533+P45K@users.noreply.github.com>
2024-03-15 20:14:26 +0100

    Dzień 9 - Przekazywanie parametrów w metodach

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -11,3 +11,16 @@ var statistics = employee.GetStatistics();
 Console.WriteLine($"Average: {statistics.Average:N2}"); // ($"łańcuchtxt {wyrażenie}") - interpolacja stringów - pozwala wstawiać wartości zmiennych bezpośrednio do łańcuchów tekstowych (stringów), "N2" - to formatowanie liczby zmiennoprzecinkowej, w tym przypadku liczba ma być sformatowana do 2 miejsc po przecinku
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
+
+
+// Parametry wartościowe i referencyjne w metodach są przekazywane zawsze przez wartość: dla typów wartościowych przez wartośc typu wartościowego, a dla typów referencyjnych przez wartość referencji
+
+/*  SetStat(Statistics statistics) // typ referencyjny i przekazanie wartości przez referencję
+    {
+        statistics = new Statistics();
+    }
+
+    public void Addgrade(float grade) // typ wartościowy i przekazanie wartości
+    {
+        this.grades.Add(grade);
+    } */
