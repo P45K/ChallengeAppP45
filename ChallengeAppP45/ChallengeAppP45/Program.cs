7ee8ea17941a32ded22b635340f754fd976ee939
P45K <160267533+P45K@users.noreply.github.com>
2024-03-21 16:01:39 +0100

    Dzień 12 - Komunikacja z użytkownikiem

--- ChallengeAppP45/ChallengeAppP45/Program.cs
+++ ChallengeAppP45/ChallengeAppP45/Program.cs
@@ -1,14 +1,12 @@
 ﻿using ChallengeAppP45;
 
-var employee = new Employee("Motoko", "Kusanagi", 1995);
-employee.AddGrade(2.22f);
-employee.AddGrade(0.01);
-employee.AddGrade('C');
-employee.AddGrade("10");
-employee.AddGrade('F');
-employee.AddGrade(-20);
-employee.AddGrade("lipa");
-
+Console.WriteLine("Witaj w programie do oceny pracowników");
+Console.WriteLine("======================================");
 Console.WriteLine();
+Console.WriteLine("Podaj ocenę pracownika: ");
+var input = Console.ReadLine();
+
+var employee = new Employee("Motoko", "Kusanagi", 1995);
+employee.AddGrade(input);
 var statistics = employee.GetStats();
-Console.WriteLine(statistics.AverageLetter);
+Console.Write($"Średnia ocena pracownika: [ {statistics.Average} ]   Klasa pracownika: [ {statistics.AverageLetter} ]");
